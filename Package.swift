70a40b26e1cc04795005df2b2fe441d8e542e410
Mathias Dietrich <mathias.dietrich@tech41.de>
2024-08-23 07:47:25 +0200

    1.2

--- Package.swift
+++ Package.swift
@@ -12,28 +12,16 @@ let package = Package(
             .macCatalyst(.v14),
         ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "Calls-Swift",
             targets: ["Calls-Swift"]),
     ],
     dependencies: [
-            // LK-Prefixed Dynamic WebRTC XCFramework
-           //.package(url: "https://github.com/apple/swift-openapi-urlsession", from: "1.0.0"),
-           // .package(url: "https://github.com/apple/swift-openapi-generator", from: "1.0.0"),
-           // .package(url: "https://github.com/apple/swift-openapi-runtime", from: "1.0.0"),
         ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "Calls-Swift",
             dependencies: [
-              // .product(name: "OpenAPIRuntime", package: "swift-openapi-runtime"),
-              // .product(name: "OpenAPIURLSession", package: "swift-openapi-urlsession"),
-            ],
-            plugins: [
-               // .plugin(name: "OpenAPIGenerator", package: "swift-openapi-generator"),
             ]
         )
     ],
