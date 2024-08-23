629cce3d2cbaddbd1f70b0aa087b556487ce6016
Mathias Dietrich <mathias.dietrich@tech41.de>
2024-08-23 07:50:09 +0200

    1.2

--- Package.swift
+++ Package.swift
@@ -1,6 +1,5 @@
-// swift-tools-version: 5.10
+// swift-tools-version: 5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
-// 1.1
 
 import PackageDescription
 
