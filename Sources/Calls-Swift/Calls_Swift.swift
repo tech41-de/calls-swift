be2c150110474ddcab02c26cfcbab8c5770cb2ae
Mathias Dietrich <mathias.dietrich@tech41.de>
2024-08-26 14:40:14 +0200

    removed debug prints

--- Sources/Calls-Swift/Calls_Swift.swift
+++ Sources/Calls-Swift/Calls_Swift.swift
@@ -153,10 +153,10 @@ public class Calls{
     
     public struct CloseTracksRequest : Codable{
         public var sessionDescription:SessionDescription
-        public var tracks: LocalTrack
+        public var tracks: [LocalTrack]
         public var force : Bool
         
-        public init(sessionDescription:SessionDescription, tracks: LocalTrack, force : Bool){
+        public init(sessionDescription:SessionDescription, tracks: [LocalTrack], force : Bool){
             self.sessionDescription = sessionDescription
             self.tracks = tracks
             self.force = force
